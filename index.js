0119925e1abd2d3810da9c6e1d8852872de66ade
Guy Sopher <guyso@wix.com>
2016-10-26 18:19:51 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- * waterlock-local-auth
- * Copyright(c) 2014 David Rivera <david.r.rivera193@gmail.com>
+ * waterlock-phone-sms-auth
+ * Copyright(c) 2016 Guy Sopher <GuySopher@gmail.com>
  * MIT Licensed
  */
 module.exports = require('./lib/waterlock-phone-sms-auth');
